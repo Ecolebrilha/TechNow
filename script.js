33d54477ae50a7c4ee1aead204be6e2dea833cd3
Ecolebrilha <144856681+Ecolebrilha@users.noreply.github.com>
2024-10-22 10:15:29 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const header = document.querySelector('header');
+    const footer = document.querySelector('footer');
+
+
+    header.style.backgroundColor = '#006400'; d9ffdf
+    footer.style.backgroundColor = 'white';
+
+
+    const logo = document.getElementById('logoImg');
+    logo.src = 'imagens/computador.jfif';
+});
